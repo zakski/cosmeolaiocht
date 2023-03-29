2f08780c99b27467ac77538deed5b3077a2c99ec
zakski <zak.szadowski+git@gmail.com>
2023-03-29 11:33:59 +0100

    Add aspects

--- pantheon-gen/src/main/kotlin/cosmeolaiocht/app/App.kt
+++ pantheon-gen/src/main/kotlin/cosmeolaiocht/app/App.kt
@@ -3,7 +3,7 @@ package cosmeolaiocht.app
 import com.szadowsz.cosmeolaiocht.PantheonProcessor
 
 fun main() {
-    val pantheons = PantheonProcessor.process("./data/pantheons","./data/roles")
+    val pantheons = PantheonProcessor.process("./../data/pantheons","./../data/roles")
 
     // for human-readable debugging purposes
     pantheons.printAspectReports()
